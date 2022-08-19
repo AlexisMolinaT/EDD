1f7a03252ed57a25b0a4bc702f4da5ed7bab67e2
AlexisMolinaT <104458053+AlexisMolinaT@users.noreply.github.com>
2022-08-19 11:29:44 -0600

    Add files via upload

---
+++ EVA1-1-STACK/src/SPackage/EVA11STACK.java
@@ -0,0 +1,20 @@
+package SPackage;
+public class EVA11STACK {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        A();
+    }
+    
+    public static void A(){
+        B();
+    }
+    
+    public static void B(){
+        int i = 1;
+        double o = 10.0;
+    }
+}
---
+++ EVA1-2-STACKOVERFLOW/src/eva1/pkg2/stackoverflow/Eva12StackOverflow.java
@@ -0,0 +1,15 @@
+package eva1.pkg2.stackoverflow;
+public class Eva12StackOverflow {
+    public static void main(String[] args) {
+        // TODO code application logic here
+        A();
+    }
+    
+    public static void A(){
+        B();
+    }
+    
+    public static void B (){
+        A();
+    }
+}
