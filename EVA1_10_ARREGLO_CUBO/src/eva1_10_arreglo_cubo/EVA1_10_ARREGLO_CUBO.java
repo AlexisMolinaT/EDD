f1221e0a279400edbab97aece58cd0fdc81dfa35
AlexisMolinaT <104458053+AlexisMolinaT@users.noreply.github.com>
2022-08-26 09:52:51 -0600

    Add files via upload

---
+++ EVA1_10_ARREGLO_CUBO/src/eva1_10_arreglo_cubo/EVA1_10_ARREGLO_CUBO.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eva1_10_arreglo_cubo;
+
+/**
+ *
+ * @author Alexis M.
+ */
+public class EVA1_10_ARREGLO_CUBO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int[][][] cuboDatos = new int[3][4][5];
+        
+        for (int i = 0; i < cuboDatos.length; i++) {
+            for (int j = 0; j < cuboDatos[i].length; j++) {
+                for (int k = 0; k < cuboDatos[i][j].length; k++) {
+                    cuboDatos[i][j][k] = 50;
+                    
+                }
+                
+            }
+            
+        }
+        
+        System.out.println("");
+        System.out.println("MANERA VISUAL DE VERLO: ");
+        for (int i = 0; i < cuboDatos.length; i++) { //PRIMER DIEMENSION (3 FILAS)
+        for (int j = 0; j < cuboDatos[i].length; j++) { //SEGUNDA DIMENSION (5 COLUMNAS)
+            for (int k = 0; k < cuboDatos[i][j].length; k++){
+                System.out.print("[" + cuboDatos[i][j][k] + "]");
+    }
+            System.out.println("");
+        }
+            System.out.println("");
+    
+}
+    }
+}
