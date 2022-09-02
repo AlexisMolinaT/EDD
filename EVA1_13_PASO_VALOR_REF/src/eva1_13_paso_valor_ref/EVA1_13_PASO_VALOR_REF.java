c135109f86907bd2d28ef6ef94c9d0d82c474824
AlexisMolinaT <104458053+AlexisMolinaT@users.noreply.github.com>
2022-09-02 09:51:49 -0600

    Add files via upload

---
+++ EVA1_13_PASO_VALOR_REF/src/eva1_13_paso_valor_ref/EVA1_13_PASO_VALOR_REF.java
@@ -0,0 +1,39 @@
+package eva1_13_paso_valor_ref;
+
+/**
+ *
+ * @author Alexis M.
+ */
+public class EVA1_13_PASO_VALOR_REF {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int valorPrueba = 100;
+        System.out.println("Valor = " + valorPrueba);
+        incVal(valorPrueba);
+        System.out.println("Valor incrementado = " + valorPrueba);
+        //-------------------------
+        PruebaValor prueba = new PruebaValor();
+        prueba.x = 100;
+        System.out.println("Valor (objeto) = " + prueba.x);
+        incVal(prueba);
+        System.out.println("Valor incrementado (objeto) = " + prueba.x);
+    }
+    
+    
+    public static void incVal(int valor){
+        valor++;
+    }
+    
+    public static void incVal(PruebaValor pruebaVal){
+        pruebaVal.x++;
+    }
+    
+}
+
+class PruebaValor{
+    int x;
+}
